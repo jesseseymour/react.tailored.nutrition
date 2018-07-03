576fedf880fe943c593aa9d7209bd3a34711a141
jesseseymour <jesse.seymour@catapultmarketing.com>
2018-07-03 14:18:29 -0400

    edited copy on StepSelectorComponent

--- src/app/home/StepSelectorComponent.js
+++ src/app/home/StepSelectorComponent.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const StepSelector = ({next, prev, nextStep, prevStep, isReadyToAdvance, results, getResults}) => {
-  const prevButton = prev ? <button onClick={prevStep}>Previous Step</button> : null
-  const nextButton = next ? <button disabled={!isReadyToAdvance} onClick={nextStep}>Next Step</button> : null
-  const resultsButton = results ? <button onClick={getResults}>Submit</button> : null
+  const prevButton = prev ? <button onClick={prevStep}>Back</button> : null
+  const nextButton = next ? <button disabled={!isReadyToAdvance} onClick={nextStep}>Next</button> : null
+  const resultsButton = results ? <button onClick={getResults}>Get Results</button> : null
   return (
     <div>
       {prevButton}
