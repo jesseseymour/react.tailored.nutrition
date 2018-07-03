98261e5a09ff9a7092e760ba011cb475a87b6b80
jesseseymour <jesse.seymour@catapultmarketing.com>
2018-07-03 14:18:57 -0400

    Added "My pet's name is:" copy to PetDetailsComponent

--- src/app/home/PetDetailsComponent.js
+++ src/app/home/PetDetailsComponent.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const PetDetails = ({styles, petDetails, handleUpdatePetName}) => {
+const PetDetails = ({styles, petDetails, handleUpdatePetName, petType = "dog"}) => {
   return (
     <div style={styles}>
-      <input type="text" name="name" required value={petDetails.name} onChange={(e) => handleUpdatePetName(e.target.value)} />
+      My {`${petType}'s`} name is: <input type="text" name="name" required value={petDetails.name} onChange={(e) => handleUpdatePetName(e.target.value)} />
     </div>
   )
 }
