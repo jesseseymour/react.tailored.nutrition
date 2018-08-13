116f86c0f8574bc68e46d703de50a38f2997f2a7
jesseseymour <jesse.seymour@catapultmarketing.com>
2018-08-13 15:23:19 -0400

    removed webpack.config.js and heroku files

--- express.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-let port = process.env.PORT || 3000
-app.use(express.static('./dist'))
-app.get('*', function (request, response, next) {
-  response.sendFile(__dirname + '/dist/index.html');
-});
-console.log(port)
-app.listen(port)
