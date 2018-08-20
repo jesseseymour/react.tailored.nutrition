6609eb96242f4ba945125f0c26350715db650376
jesseseymour <jesse.seymour@catapultmarketing.com>
2018-08-20 10:50:31 -0400

    added 25 character max length to pet name input

--- src/app/home/PetDetailsComponent.js
+++ src/app/home/PetDetailsComponent.js
@@ -4,7 +4,7 @@ const PetDetails = ({styles, petDetails, handleUpdatePetName, petType = "dog", s
   return (
     <div style={styles}>
       <div className="tntool__banner tntool__banner__input">My {`${petType.charAt(0).toUpperCase() + petType.substr(1).toLowerCase()}'s`} Name is:</div>
-      <input className="tntool__input tntool__input--name" type="text" name="name" required value={petDetails.name} onFocus={() => scrollTo(".tntool__banner--finder", 1000)} onChange={(e) => handleUpdatePetName(e.target.value)} />
+      <input className="tntool__input tntool__input--name" maxLength="25" type="text" name="name" required value={petDetails.name} onFocus={() => scrollTo(".tntool__banner--finder", 1000)} onChange={(e) => handleUpdatePetName(e.target.value)} />
     </div>
   )
 }
